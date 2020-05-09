247e743eccb767d662160626274a732a5476a0b5
Keriann <keriann.lin@gmail.com>
2020-05-09 15:08:50 -0400

    mocked data using react, rendered fake data to page

--- client/App.jsx
+++ client/App.jsx
@@ -10,14 +10,19 @@
  */
 
 import React, { Component } from 'react';
+import Queries from './components/Queries.jsx';
+// import store from './store.js';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-        <div>
+        <div className="main">
           HELLO WORLD
+          <div>
+            <Queries />
+          </div>
         </div>
     );
   }
---
+++ client/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import plantsReducer from './plantsReducer';
+
+export default combineReducers({
+  // reducers go here, currently in destructured format
+})
