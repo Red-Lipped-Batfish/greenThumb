0505f6311f46e0fd2fb58d7c1cd0f66f1b58cbf0
Tommy Han <tommy@Tommys-Air.home>
2020-05-09 12:56:27 -0400

    added bodyParser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 const PORT = 3000;
@@ -8,12 +9,14 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
   console.log(req.body);
   res.send('recieved your request!');
 });
 
 app.get('/', (req, res) => {
+  console.log('hello world');
+
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
