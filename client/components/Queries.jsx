0007c1f0cfe20de5c6dda2ef73a1e46600cb4f3a
Tommy Han <tommy@Tommys-Air.home>
2020-05-11 19:27:37 -0400

    fixed react child issue.

--- client/components/Queries.jsx
+++ client/components/Queries.jsx
@@ -14,77 +14,78 @@ import { connect } from 'react-redux';
 import { submitSelection } from '../actions/actions';
 import RowQueries from './RowQueries.jsx';
 
-
 const mapStateToProps = state => ({
-  listofPlants: state.plantsReducer.arrayOfPlants
+  listofPlants: state.plantsReducer.arrayOfPlants,
   // state here
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   // add dispatchers here
-  submitSelection: (data) => {
+  submitSelection: data => {
     // console.log('Queries: mapDispatchToProps, submitSelection, data[0] - ', data[0])
-    dispatch(submitSelection(data))
-  }
-})
-
-
+    dispatch(submitSelection(data));
+  },
+});
 
 class Queries extends Component {
-
   render() {
     const fetchData = () => {
-      const waterRetention = document.querySelector("#water-retention").value;
+      const waterRetention = document.querySelector('#water-retention').value;
       // console.log('value of waterRetention is: ',waterRetention);
-      const resproutAbility = document.querySelector("#resprout-ability").value;
+      const resproutAbility = document.querySelector('#resprout-ability').value;
       // console.log('value of resproutAbility is: ', resproutAbility);
       // const growthRate = document.querySelector("#growth-rate").value;
       // const lifespan = document.querySelector("#resprout-ability").value;
       // const bloomPeriod = document.querySelector("#bloom-period").value;
       const url = `/api/getManyPlants?drought_tolerance=${waterRetention}&resprout_ability=${resproutAbility}`;
-      console.log('url is: ', url)
+      console.log('url is: ', url);
       fetch(url)
         .then(res => res.json())
         .then(data => {
-          const limitedData = data.slice(0, 50)
+          const limitedData = data.slice(0, 50);
           console.log('Queries: fetch(url)  : ', limitedData);
-          this.props.submitSelection(limitedData)
+          this.props.submitSelection(limitedData);
           return;
-        })
-    }
+        });
+    };
 
-    
     // separate by 2
     const rowQueries = [];
     for (let i = 0; i < this.props.listofPlants.length; i += 2) {
       const individualRow = [];
-      individualRow.push(this.props.listofPlants[i])
-      individualRow.push(this.props.listofPlants[i+1])
-      rowQueries.push(<RowQueries key={'row' + i} columnCards={individualRow} />);
+      // individualRow.push(this.props.listofPlants[i]);
+      // individualRow.push(this.props.listofPlants[i + 1]);
+      rowQueries.push(
+        <RowQueries key={'row' + i} columnCards={this.props.listofPlants[i]} />
+      );
     }
 
     return (
       <div>
-      <section className="main-query-section">
+        <section className="main-query-section">
           <img src="https://i.ibb.co/2cQQMjz/a011787e63ea6f6b2d1879b94ef2ca31-removebg-preview-1.png" />
-      </section>
-      <section className="title">
-        <h2>Find Your Perfect Plant</h2>
-      </section>
-      <div className="query-selections">
+        </section>
+        <section className="title">
+          <h2>Find Your Perfect Plant</h2>
+        </section>
+        <div className="query-selections">
           <div>
             <label htmlFor="water-retention">Water Retention</label>
             <select id="water-retention">
-              <option value="" disabled>Water Retention</option>
-                <option value="High">High</option>
-                <option value="Medium">Medium</option>
-                <option value="Low">Low</option>
+              <option value="" disabled>
+                Water Retention
+              </option>
+              <option value="High">High</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
             </select>
             <label htmlFor="resprout-ability">Resprout Ability</label>
             <select id="resprout-ability">
-              <option value="" disabled>Resprout Ability</option>
-                <option value="true">True</option>
-                <option value="false">False</option>
+              <option value="" disabled>
+                Resprout Ability
+              </option>
+              <option value="true">True</option>
+              <option value="false">False</option>
             </select>
             {/* <label htmlFor="growth-rate">Growth Rate</label>
             <select id="growth-rate">
@@ -112,13 +113,11 @@ class Queries extends Component {
             </select> */}
             <input type="submit" value="Submit" onClick={() => fetchData()} />
           </div>
-      </div>  
-  
-      
-      {rowQueries}
+        </div>
 
+        {rowQueries}
       </div>
-    )
+    );
   }
 }
 
--- client/components/RowQueries.jsx
+++ client/components/RowQueries.jsx
@@ -10,16 +10,38 @@
  */
 
 import React, { Component } from 'react';
-import ColumnCards from './ColumnCards.jsx'
 
-
-const RowQueries = (props) => {
-
+const RowQueries = props => {
+  console.log('props ', props);
   return (
-  <div className="row-cards-query">
-    <ColumnCards columnCards={props.columnCards} />
-  </div>
-    )
-}
+    <div className="row-cards-query">
+      <div className="column-card-query">
+        <div className="plant-card">
+          <div className="plant-card-header">
+            <h3>{props.columnCards.common_name}</h3>
+            <img className="query-plant" src={props.columnCards.image_url} />
+          </div>
+          <hr />
+          <strong>Details</strong>
+          <p>Seed period begins: {props.columnCards.seed_period_begin}</p>
+          <p>Seed period ends: {props.columnCards.seed_period_end}</p>
+          <p>Moisture use: {props.columnCards.moisture_use}</p>
+          <p>Drought tolerance: {props.columnCards.drought_tolerance}</p>
+          <p>ph maximum: {props.columnCards.ph_maximum}</p>
+          <p>ph minimum: {props.columnCards.ph_minimum}</p>
+          <p>Resprout ability: {props.columnCards.resprout_ability}</p>
+          <p>Shade tolerance: {props.columnCards.shade_tolerance}</p>
+          <p>Bloom period: {props.columnCards.bloom_period}</p>
+          <p>
+            Commercial availability: {props.columnCards.commercial_availability}
+          </p>
+          <p>Growth rate: {props.columnCards.growth_rate}</p>
+          <p>lifespan: {props.columnCards.lifespan}</p>
+          <p>Height at maturity: {props.columnCards.mature_height}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RowQueries;
