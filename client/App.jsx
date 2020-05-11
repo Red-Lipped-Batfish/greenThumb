5488c40759041150fe56833e687b25f9b81a8729
Fan <fanny.shao18@gmail.com>
2020-05-11 00:18:20 -0400

    added homepage and signup to router, edited navbar and css

--- client/App.jsx
+++ client/App.jsx
@@ -12,7 +12,8 @@
 import React, { Component } from 'react';
 import Queries from './components/Queries.jsx';
 import NavBar from './components/NavBar.jsx';
-// import Homepage from './components/Homepage.jsx';
+import Signup from './components/Signup.jsx';
+import Homepage from './components/Homepage.jsx';
 import PlantShelf from './components/PlantShelf.jsx';
 // import store from './store.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -25,9 +26,10 @@ class App extends Component {
         <NavBar />
         <div>
           <Switch>
-            {/* <Route exact path="/" component={ Homepage } /> */}
+            <Route exact path="/" component={ Homepage } />
             <Route exact path="/query" component={ Queries } />
             <Route exact path="/plantshelf" component={ PlantShelf } />
+            <Route exact path="/signup" component={ Signup } />
             {/* <Route exact path="/profile" component={ Profile } /> */}
           </Switch>
         </div>
--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -15,19 +15,20 @@
  const NavBar = () => {
    return (
 
-     <nav>
-     <ul className="frontnav">
-       <li>
-         <Link to='/query'>Query</Link>
-       </li>
-       <li>
-         <Link to='/plantshelf'>Plant Shelf</Link>
-       </li>
-       <li>
-         <Link to='/profile'>Profile</Link>
-       </li>
-     </ul>
-   </nav>
+    <nav className="frontnav">
+        <nav className="leftnav">
+          <span className="navlink"><Link to="/">GREEN THUMB</Link></span>
+          <span className="navlink"><Link to="/signup">Sign Up</Link></span>
+          <span className="navlink"><Link to="/query">Make a query</Link></span>
+          <span className="navlink"><Link to="/plantshelf">Plantshelf</Link></span>
+        </nav>
+        <nav className="rightnav">
+          <input className="login" type="text" placeholder="Username" required />
+          <input className="login" type="text" placeholder="Password" required />
+          <button className="login">Login</button>
+          
+        </nav>
+      </nav>
      )
  }
 
